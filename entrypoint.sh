f7a0e76af7fc7683b41055703eff67fe59a2a17e
Ramene Anthony <anthonyramene@gmail.com>
2019-09-15 08:27:03 -0400

    v0.0.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,9 @@ set -exuo pipefail
 echo "$1"
 time=$(date)
 
+k8s_config="${K8S_CONFIG}"
+
 env
 
 echo ::set-output name=time::$time
-echo ::set-output name=k8s_config::$K8S_CONFIG
+echo ::set-output name=k8s_config::$k8s_config
