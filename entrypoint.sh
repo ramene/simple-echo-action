551b23801f421a6f068d109c307d15f46f4b3724
Ramene Anthony <anthonyramene@gmail.com>
2019-09-15 07:01:39 -0400

    v0.0.3

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,6 @@
 
 set -e
 
-if [ ! -z "$GITHUB_WORKFLOW" ]; then
-    echo "Exported KUBECONFIG >>>>>>>>>: ${K8S_CONFIG}"
-else
-    "Theory disproven!"
-fi
-
-if [ ! -z "${GITHUB_EVENT_NAME}" ]; then
-    echo "GitHub Event Type >>>>>>>>>>>>>: ${GITHUB_EVENT_NAME}"
-else
-    echo "No cigar"
-fi
-
 echo "$1"
 time=$(date)
 
