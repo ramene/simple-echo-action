6fac6cc9e419f9b67d1fcb1cf69a6e754d061d20
Ramene Anthony <anthonyramene@gmail.com>
2019-09-15 04:11:46 -0400

    v0.0.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/sh -l
 
 set -exuo pipefail
- 
+ ${PULUMIFY_BUILD:-}
+echo "${PULUMI_ROOT:-}"
+echo "\`${PULUMI_ROOT:-}\`"
+echo "${GITHUB_EVENT_NAME:-}"
+echo "\`${GITHUB_EVENT_NAME:-}\`"
+
 if [ ! -z "${PULUMI_ROOT}" ]; then
     echo "\`${PULUMI_ROOT}\`"
 else
     echo "No cigar"
 fi
+
+PULUMI_CI_SYSTEM
 
 echo "$1"
 time=$(date)
