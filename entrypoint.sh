a1ef45a033c048cb81b2e84c337f6e4d90e96406
Ramene Anthony <anthonyramene@gmail.com>
2019-09-15 19:53:24 -0400

    v0.0.7

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ env
 echo "$1"
 time=$(date)
 
-echo ">>>>>>>>>>>>>>>>>>>>>> $RUNNER_WORKSPACE"
-tmp="^^^^^^^^ asset values ^^^^^^^^"
+echo "$RUNNER_WORKSPACE/kubeconfig"
+tmp ="^^^^^^^ asset values ^^^^^^^"
 echo ::set-output name=time::$time
 echo ::set-output name=tmp::$tmp
