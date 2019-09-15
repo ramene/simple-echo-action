2725e8650b12eef82729bb42c575a32f1a1c13a0
Ramene Anthony <anthonyramene@gmail.com>
2019-09-15 05:37:11 -0400

    v0.0.3

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,13 @@
 set -e
 
 if [ ! -z "$GITHUB_WORKFLOW" ]; then
-    echo "Pulumi Access Token: ${PULUMI_CI_SYSTEM}"
-    echo "Pulumi Command: ${PULUMI_COMMAND}"
+    echo "Exported KUBECONFIG >>>>>>>>>: ${KUBECONFIG}"
 else
     "Theory disproven!"
 fi
 
 if [ ! -z "${GITHUB_EVENT_NAME}" ]; then
-    echo "GitHub Event Type: ${GITHUB_EVENT_NAME} ${PULUMI_CI_PULL_REQUEST_SHA}"
+    echo "GitHub Event Type >>>>>>>>>>>>>: ${GITHUB_EVENT_NAME}"
 else
     echo "No cigar"
 fi
