31ae0035fce457c85b66ed04b49f467ec06eee80
Ramene Anthony <anthonyramene@gmail.com>
2019-09-15 05:19:05 -0400

    v0.0.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,14 @@
 set -e
 
 if [ ! -z "$GITHUB_WORKFLOW" ]; then
-    echo "Pulumi Access Token: ${LC_CTYPE}"
+    echo "Pulumi Access Token: ${PULUMI_CI_SYSTEM}"
+    echo "Pulumi Command: ${PULUMI_COMMAND}"
 else
     "Theory disproven!"
 fi
 
 if [ ! -z "${GITHUB_EVENT_NAME}" ]; then
-    echo "GitHub Event Name: ${GITHUB_EVENT_NAME}"
+    echo "GitHub Event Type: ${GITHUB_EVENT_NAME} ${PULUMI_CI_PULL_REQUEST_SHA}"
 else
     echo "No cigar"
 fi
