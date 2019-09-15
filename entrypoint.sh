0dc380f030ceaf3a0313c4e2b10c2562506a6815
Ramene Anthony <anthonyramene@gmail.com>
2019-09-15 03:28:33 -0400

    v0.0.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
+if [ ! -z "$PULUMI_ROOT" ] && [ "$PULUMI_ROOT" != "null" ]; then
+    echo "\`PULUMI_ROOT\`"
+else
+    echo "No cigar"
+fi
+
 echo "$1"
 time=$(date)
 echo ::set-output name=time::$time
