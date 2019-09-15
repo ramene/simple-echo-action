58cfed5b1b6edfbc37dad46860dd5a1172ef5b58
Ramene Anthony <anthonyramene@gmail.com>
2019-09-15 07:56:49 -0400

    v0.0.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,5 @@ time=$(date)
 
 env
 
-echo $K8S_CONFIG
-# echo $(cat $GITHUB_EVENT_PATH)
-# export K8S_CONFIG='testing passing values around'
-
 echo ::set-output name=time::$time
 echo ::set-output name=k8s_config::$K8S_CONFIG
