f7184244e70620333aebd7df36056a6f2223d1df
Ramene Anthony <anthonyramene@gmail.com>
2019-09-15 05:06:57 -0400

    v0.0.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@
 set -e
 
 if [ ! -z "$GITHUB_WORKFLOW" ]; then
-    echo "\`${KUBECONFIG}\`"
+    echo "Pulumi Access Token: ${LC_CTYPE}"
 else
     "Theory disproven!"
 fi
 
 if [ ! -z "${GITHUB_EVENT_NAME}" ]; then
-    echo "\`${GITHUB_EVENT_NAME}\`"
+    echo "GitHub Event Name: ${GITHUB_EVENT_NAME}"
 else
     echo "No cigar"
 fi
