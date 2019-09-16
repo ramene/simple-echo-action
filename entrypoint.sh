7a74895313a28bef1e13792f2b8c0dce00b6418f
Ramene Anthony <anthonyramene@gmail.com>
2019-09-15 21:21:42 -0400

    v0.0.7

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,10 @@ echo "Do I see you?: >> ${OUTPUT_BROADCAST}"
 echo "Original INPUT_BROADCAST: >> ${INPUT_BROADCAST}"
 echo "\`$INPUT_BROADCAST\`"
 
-OUTPUT_BROADCAST=${OUTPUT_KUBECONFIG}
-echo " NEW >>>>>>>>>>>>>>>>>>>>  \`${OUTPUT_BROADCAST}\'"
+set INPUT_BROADCAST="WTF!"
+
+set OUTPUT_BROAD="${INPUT_BROADCAST}"
+echo " NEW >>>>>>>>>>>>>>>>>>>>  \`${OUTPUT_BROAD}\'"
 
 echo ::set-output name=time::$time
 echo ::set-output name=tmp::$tmp
