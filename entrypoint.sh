06cbe35d3a2abfaa92486126fc07db024fab8277
Ramene Anthony <anthonyramene@gmail.com>
2019-09-15 20:46:31 -0400

    v0.0.7

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,11 @@ env
 echo "$1"
 time=$(date)
 
-echo ">>>>>>>>>>>>>>>>>>>> $GITHUB_WORKSPACE"
+echo "Original INPUT_BROADCAST: ${INPUT_BROADCAST}"
+echo "\`$INPUT_BROADCAST\`"
 
+INPUT_BROADCAST = "\`${GITHUB_SHA}\`"
+echo " NEW >>>>>>>>>>>>>>>>>>>>  \`${INPUT_BROADCAST}\'"
 
 echo ::set-output name=time::$time
 echo ::set-output name=tmp::$tmp
