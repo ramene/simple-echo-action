a25b1358278a49713fdbbc4e0286b64a77b4e13d
Ramene Anthony <anthonyramene@gmail.com>
2019-09-15 21:42:59 -0400

    v0.0.7

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,13 @@ env
 echo "$1"
 time=$(date)
 
-echo "Do I see you?: >> ${OUTPUT_KUBECONFIG}"
-echo "Do I see you?: >> ${OUTPUT_BROADCAST}"
+# echo "Do I see you?: >> ${OUTPUT_KUBECONFIG}"
+# echo "Do I see you?: >> ${OUTPUT_BROADCAST}"
 
-echo "Original INPUT_BROADCAST: >> ${INPUT_BROADCAST}"
-echo "\`$INPUT_BROADCAST\`"
-
-export INPUT_BROADCAST="WTF!"
+echo "INPUT_BROADCAST: >> ${INPUT_BROADCAST}"
 
 export OUTPUT_BROAD="${INPUT_BROADCAST}"
-echo " NEW >>>>>>>>>>>>>>>>>>>>  ${OUTPUT_BROAD}"
+
+echo " NEW >>>>>>>>>>>> INPUT_BROADCAST: <<<<<<<< ${OUTPUT_BROAD}"
 
 echo ::set-output name=time::$time
