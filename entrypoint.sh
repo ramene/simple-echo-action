3f62ae9cf7a3a10f53b2592b5c4ad208db458e32
Ramene Anthony <anthonyramene@gmail.com>
2019-09-17 04:48:34 -0400

    v0.0.14

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,15 @@ time=$(date)
 
 pass_var=$INPUT_SOME_VAR
 
+# If the PULUMI_CI variable is set, we'll do some extra things to make common tasks easier.
+if [ ! -z "$GITHUB_ACTION" ]; then
+    root_path="$GITHUB_WORKSPACE"
+    echo "GITHUB_WORKSPACE path is: ${root_path}"
+    kubeconfig_path="$GITHUB_WORKSPACE/.kube"
+    echo "kubeconfig path is: ${kubeconfig_path}"
+fi
+
+echo $(cat ${kubeconfig_path}/out.log)
+
 echo ::set-output name=time::$time
 echo ::set-output name=foo::$pass_var
