90345e6b1a225b573e685a0d8c17ac80d168f24f
Ramene Anthony <anthonyramene@gmail.com>
2019-09-18 05:10:51 -0400

    v0.0.15

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,21 +3,22 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -exou pipefail
 
-env
+# If the PULUMI_CI variable is set, we'll do some extra things to make common tasks easier.
+if [ ! -z "$GITHUB_ACTION" ]; then
+
+    env
+    
+    root_path="$GITHUB_WORKSPACE"
+    echo "GITHUB_WORKSPACE path is: ${root_path}"
+    pulumi_ci="$GITHUB_WORKSPACE/.pulumi"
+    echo "pulumi_ci path is: ${pulumi_ci}"
+fi
 
 time=$(date)
 
 pass_var=$INPUT_SOME_VAR
 
-# If the PULUMI_CI variable is set, we'll do some extra things to make common tasks easier.
-if [ ! -z "$GITHUB_ACTION" ]; then
-    root_path="$GITHUB_WORKSPACE"
-    echo "GITHUB_WORKSPACE path is: ${root_path}"
-    kubeconfig_path="$GITHUB_WORKSPACE/.kube"
-    echo "kubeconfig path is: ${kubeconfig_path}"
-fi
-
-echo $(cat ${kubeconfig_path}/out.log)
+echo $(cat ${pulumi_ci}/output)
 
 echo ::set-output name=time::$time
 echo ::set-output name=foo::$pass_var
